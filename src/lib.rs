0e769dd8a443eb49807f1755f3adf58e5de40751
Cody P Schafer <dev@codyps.com>
2015-04-03 15:13:53 -0400

    updates!

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-#![feature(path,io,fs)]
+#![feature(core)]
 
 #[macro_use]
 extern crate log;
 
-extern crate "rustc-serialize" as rustc_serialize;
+extern crate rustc_serialize;
 #[macro_use]
 extern crate err;
 
@@ -11,49 +11,24 @@ extern crate err;
 use rustc_serialize::hex;
 use std::{fs,env,io};
 
+use std::borrow::ToOwned;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
 static DUMMY_MAC: &'static str = "00:00:00:00:00:00";
 
-#[derive(Debug)]
-pub enum MacAddrError {
-    FromHex(hex::FromHexError),
-    Io(io::Error),
-    Utf8(std::string::FromUtf8Error),
-}
-
-error_enum! {
+from_enum! {
     pub enum DevPropError {
         auto Io(io::Error),
-        auto Utf8(std::string::FromUtf8Error)
+        auto Utf8(std::str::Utf8Error)
     }
 }
 
-from_error! { MacAddrError => FromHex(hex::FromHexError) }
-from_error! { MacAddrError => Io(io::Error) }
-from_error! { MacAddrError => Utf8(std::string::FromUtf8Error) }
-
-#[derive(Debug)]
-pub enum LocalMacAddrsError {
-    MacAddr(MacAddrError),
-    Io(io::Error),
-}
-
-from_error! { LocalMacAddrsError => MacAddr(MacAddrError) }
-from_error! { LocalMacAddrsError => Io(io::Error) }
-
-pub fn net_dev_mac_addr(path: &Path) -> Result<Vec<u8>, MacAddrError> {
-    let mut f = try!(fs::File::open(&path.join("address")));
-    let s_mac = {
-        let mut v = Vec::with_capacity(DUMMY_MAC.len() + 1);
-        try!(f.read_to_end(&mut v));
-        try!(String::from_utf8(v.into_iter()
-            .filter(|b| *b != b':')
-            .collect()
-        ))
-    };
-    Ok(try!(hex::FromHex::from_hex(&s_mac[..])))
+pub fn macaddr_from_str(s: &str) -> Result<Vec<u8>, hex::FromHexError> {
+    let mac : String = s.chars()
+            .filter(|b| *b != ':')
+            .collect();
+    hex::FromHex::from_hex(&mac[..])
 }
 
 pub fn netdev_address(path: &Path) -> Result<String, DevPropError> {
@@ -61,15 +36,15 @@ pub fn netdev_address(path: &Path) -> Result<String, DevPropError> {
     let s_mac = {
         let mut v = Vec::with_capacity(DUMMY_MAC.len() + 1);
         try!(f.read_to_end(&mut v));
-        try!(String::from_utf8(v))
+        try!(std::str::from_utf8(&v[..v.len()-1])).to_owned()
     };
     Ok(s_mac)
 }
 
 pub fn sysfs() -> PathBuf {
     match env::var("SYSFS_PATH") {
-        Ok(p) => PathBuf::new(&p[..]),
-        Err(_) => PathBuf::new("/sys")
+        Ok(p) => PathBuf::from(&p[..]),
+        Err(_) => PathBuf::from("/sys")
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,12 @@
 extern crate env_logger;
 
 #[test]
-fn test_netdev() {
+fn show_netdev_addrs() {
     or_panic!(env_logger::init());
-    println!("{:?}", super::local_mac_addrs())
+    println!("{:?}", super::netdev_addrs())
+}
+
+#[test]
+fn macaddr_from_str() {
+    assert_eq!(super::macaddr_from_str("00:11:22:33").unwrap(), [ 0, 0x11, 0x22, 0x33 ]);
 }
